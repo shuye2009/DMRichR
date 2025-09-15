2b2d2ec39ca37d2841ede797d167ec48a0d239a2
shuye2009 <shuye2009@gmail.com>
2025-09-15 11:04:26 -0400

    make home to search for both denovo and known motifs

--- exec/HOMER.sh
+++ exec/HOMER.sh
@@ -22,7 +22,7 @@ homerDMR(){
 	-cpg \
 	-size given \
 	-p ${cores} \
-	-nomotif"
+	-known"
 
 	echo $call
 	eval $call
@@ -39,7 +39,7 @@ homerDMR(){
 	-cpg \
 	-size given \
 	-p ${cores} \
-	-nomotif"
+	-known"
 
 	echo $call
 	eval $call
@@ -56,7 +56,7 @@ homerDMR(){
 	-cpg \
 	-size given \
 	-p ${cores} \
-	-nomotif"
+	-known"
 
 	echo $call
 	eval $call
